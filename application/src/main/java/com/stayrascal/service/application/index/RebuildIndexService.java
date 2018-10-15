4b54177f62830d6b22b8443892af2fe7383881fc
Zhiping Wu <zpwu@thoughtworks.com>
2018-10-15 23:20:30 +0800

    update application yml

--- application/src/main/java/com/stayrascal/service/application/index/RebuildIndexService.java
+++ application/src/main/java/com/stayrascal/service/application/index/RebuildIndexService.java
@@ -1,6 +1,7 @@
 package com.stayrascal.service.application.index;
 
 import com.stayrascal.service.application.component.ComponentService;
+import com.stayrascal.service.application.item.ItemService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,8 +11,12 @@ public class RebuildIndexService {
     @Autowired
     private ComponentService componentService;
 
+    @Autowired
+    private ItemService itemService;
+
     public void rebuildIndex() {
         try {
+            itemService.rebuild();
             componentService.rebuild();
         } catch (Exception e) {
             throw new RebuildIndexException("Fail to rebuild index", e);
